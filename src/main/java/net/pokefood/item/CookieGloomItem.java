5da9266e2204cc21026532dacce26bc9ed6f1607
HultraFox <70465716+HultraFox@users.noreply.github.com>
2023-03-21 10:49:59 +0100

    Add files via upload

---
+++ src/main/java/net/pokefood/item/CookieGloomItem.java
@@ -0,0 +1,17 @@
+
+package net.pokefood.item;
+
+import net.pokefood.init.PokefoodModTabs;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+
+public class CookieGloomItem extends Item {
+	public CookieGloomItem() {
+		super(new Item.Properties().tab(PokefoodModTabs.TAB_POKE_FOOD_COOKIES).stacksTo(64).rarity(Rarity.COMMON)
+				.food((new FoodProperties.Builder()).nutrition(4).saturationMod(1f).alwaysEat()
+
+						.build()));
+	}
+}
